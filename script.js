5b8e321042210333275853b990d788b76bb46bb3
NummNuttzCDXX <bbirdy2018@gmail.com>
2023-05-08 20:19:50 -0400

    Initialize number and operator variables and add operate function to run math functions

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Calculator Script
+// Initialize Variables
+let num1,
+num2,
+operator;
+
 // Operator Functions
 function add(num1, num2) {
     return num1 + num2
@@ -13,4 +19,18 @@ function mult(num1, num2) {
 
 function divide(num1, num2) {
     return num1 / num2
+}
+
+function operate(num1, operator, num2) {
+    if (operator === '+') {
+        return add(num1, num2)
+    } else if (operator === '-') {
+        return sub(num1, num2)
+    } else if (operator === '*') {
+        mult(num1, num2) 
+    } else if (operator === '/') {
+        divide(num1, num2)
+    } else { // Catch Error
+        alert('ERROR: INVALID OPERATOR')
+    }
 }
