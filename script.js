452c61aff58cbc50c0243a28f0b0d5b049dcd579
NummNuttzCDXX <bbirdy2018@gmail.com>
2023-05-08 20:09:22 -0400

    Add basic operation functions

---
+++ script.js
@@ -0,0 +1,16 @@
+// Operator Functions
+function add(num1, num2) {
+    return num1 + num2
+}
+
+function sub(num1, num2) {
+    return num1 - num2
+}
+
+function mult(num1, num2) {
+    return num1 * num2
+}
+
+function divide(num1, num2) {
+    return num1 / num2
+}
