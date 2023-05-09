416e3e2ca47aeac53fc4f76000ec1ded610ee733
NummNuttzCDXX <bbirdy2018@gmail.com>
2023-05-08 23:29:45 -0400

    Buttons now print to the viewport

--- script.js
+++ script.js
@@ -2,7 +2,11 @@
 // Initialize Variables
 let num1,
 num2,
-operator;
+operator
+
+const numBtns = document.querySelectorAll('.num button'),
+opBtns = document.querySelectorAll('.ops button')
+vPort = document.querySelector('.view')
 
 // Operator Functions
 function add(num1, num2) {
@@ -34,3 +38,50 @@ function operate(num1, operator, num2) {
         alert('ERROR: INVALID OPERATOR')
     }
 }
+
+// Event listener to update the viewport when a NUMBER button is pressed
+numBtns.forEach((btn) => {
+    if (btn.id === 'equal') {
+        //TEMP
+        btn.addEventListener('click', () => {
+            vPort.textContent += btn.textContent
+        })
+    } else {
+        btn.addEventListener('click', () => {
+            vPort.textContent += btn.textContent
+        })
+    }
+})
+
+// Event listener to run operations 
+opBtns.forEach((btn) => {
+    if (btn.id === 'add') {
+        btn.addEventListener('click', () => {
+            operator = 'plus'
+            num1 = vPort.textContent
+            vPort.textContent += btn.textContent
+        })
+    } else if (btn.id ==='sub') {
+        btn.addEventListener('click', () => {
+            operator = 'minus'
+            num1 = vPort.textContent
+            vPort.textContent += btn.textContent
+        })
+    } else if (btn.id === 'mult') {
+        btn.addEventListener('click', () => {
+            operator = 'times'
+            num1 = vPort.textContent
+            vPort.textContent += btn.textContent
+        })
+    } else if (btn.id === 'div') {
+        btn.addEventListener('click', () => {
+            operator = 'divide'
+            num1 = vPort.textContent
+            vPort.textContent += btn.textContent
+        })
+    } else if (btn.id === 'clear') {
+        btn.addEventListener('click', () => {
+            vPort.textContent = ''
+        })
+    } 
+})
